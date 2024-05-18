7f793f12304b36fbf35bdfe2c1b7417b3e9c8439
Егор Щукин <e.v.schukin@yandex.ru>
2024-05-18 19:31:45 +0300

    Remove unused imports

--- python/server.py
+++ python/server.py
@@ -1,7 +1,6 @@
-import sys
 import uvicorn
 
-from fastapi import FastAPI, Form, Request
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
