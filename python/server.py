37ad8f1dde748b0ce7108459cfba4a158933c3d8
Егор Щукин <e.v.schukin@yandex.ru>
2024-05-18 19:30:15 +0300

    Add patology list; create global server access

--- python/server.py
+++ python/server.py
@@ -1,20 +1,53 @@
+import sys
+import uvicorn
+
 from fastapi import FastAPI, Form, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-
 from pathlib import Path
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    BASE_URL = "http://localhost:8000"
+    USE_NGROK = True
+
 
 BASE_DIR = str(Path(__file__).resolve().parent.parent)
+settings = Settings()
 app = FastAPI()
+
+
+def init_webhooks(base_url):
+    # Update inbound traffic via APIs to use the public-facing ngrok URL
+    pass
+
+
+if settings.USE_NGROK:
+    from pyngrok import ngrok
+    port = "8000"
+    public_url = ngrok.connect(port).public_url
+    settings.BASE_URL = public_url
+    init_webhooks(public_url)
+    print(public_url)
+
+
 app.mount(
-    BASE_DIR + "/static",
-    StaticFiles(directory=BASE_DIR + "/static"),
+    "/static",
+    StaticFiles(directory=BASE_DIR + "\\static"),
     name="static",
 )
-templates = Jinja2Templates(directory=BASE_DIR + "/templates")
-chosen_values = {'Сердце': 'норма'}
-values = {'Сердце': ['норма', 'порок 1', 'порок 2']}
+templates = Jinja2Templates(directory=BASE_DIR + "\\templates")
+chosen_values = {'cor': 'Норма, CN', 'pulm': 'Норма, LN'}
+with open('python/patology.txt', 'r') as all_patologies:
+    values = {}
+    list_patologies = [i.replace('\n', '').split(',') for i in all_patologies.readlines()]
+    for patology in list_patologies:
+        if patology[-1] not in values.keys():
+            values[patology[-1]] = [', '.join(patology[:-1])]
+        else:
+            values[patology[-1]].append(', '.join(patology[:-1]))
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -26,8 +59,9 @@ async def index(request: Request):
 
 
 @app.post("/handle-change")
-async def handle_change(request: Request, value: str = Form(...)):
-    chosen_values['Сердце'] = value
+async def handle_change(request: Request):
+    form_data = await request.form()
+    chosen_values = dict(form_data)
     return templates.TemplateResponse(
         "index.html",
         {"request": request, "chosen": chosen_values, "values": values}
@@ -37,3 +71,6 @@ async def handle_change(request: Request, value: str = Form(...)):
 @app.get("/current-values")
 async def handle_valyes(request: Request):
     return chosen_values
+
+
+uvicorn.run(app, host="localhost", port=8000)
